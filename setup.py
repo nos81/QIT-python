f00cf0f5462391d830b13a6158f14550d7a84698
Ville Bergholm <ville.bergholm@iki.fi>
2014-04-09 23:50:06 +0200

    Oops, missing comma added.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(name             = 'qit',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
